451863c47be3e971f9e1bc5e3fc511fde5c0bbbf
Kai Kim <juhyeon1114@gmail.com>
2022-12-12 21:59:18 +0900

    aop구현3

--- src/test/java/hello/springaop/AopTest.java
+++ src/test/java/hello/springaop/AopTest.java
@@ -4,6 +4,7 @@ import hello.springaop.order.OrderRepository;
 import hello.springaop.order.OrderService;
 import hello.springaop.order.aop.AspectV1;
 import hello.springaop.order.aop.AspectV2;
+import hello.springaop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
-@Import(AspectV2.class) //추가
+@Import(AspectV3.class) //추가
 @SpringBootTest
 public class AopTest {
 
