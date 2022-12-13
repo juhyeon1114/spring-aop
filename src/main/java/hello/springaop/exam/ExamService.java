2e0ce6a2e2e86ce327ab33f5182e210b02417322
Kai Kim <juhyeon1114@gmail.com>
2022-12-13 09:04:28 +0900

    aop 실전 예제

---
+++ src/main/java/hello/springaop/exam/ExamService.java
@@ -0,0 +1,18 @@
+package hello.springaop.exam;
+
+import hello.springaop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+
+}
---
+++ src/test/java/hello/springaop/ExamTest.java
@@ -0,0 +1,25 @@
+package hello.springaop;
+
+import hello.springaop.exam.ExamService;
+import hello.springaop.exam.aop.RetryAspect;
+import hello.springaop.exam.aop.TraceAspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import({TraceAspect.class, RetryAspect.class})
+@SpringBootTest
+public class ExamTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            examService.request("data" + i);
+        }
+    }
+
+}
