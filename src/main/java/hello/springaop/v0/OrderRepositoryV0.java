d14dc09125e01dffdfa5fbdaceef6215407130eb
juhyeon <juhyeon1114@gmail.com>
2023-12-25 16:37:46 +0900

    Init, 기본 구조 (v0)

---
+++ src/main/java/hello/springaop/v0/OrderRepositoryV0.java
@@ -0,0 +1,26 @@
+package hello.springaop.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        // save logic
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/hello/springaop/v0/OrderServiceV0.java
@@ -0,0 +1,17 @@
+package hello.springaop.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+
+    private final OrderRepositoryV0 orderRepositoryV0;
+
+    public void orderItem(String itemId) {
+        orderRepositoryV0.save(itemId);
+    }
+
+}
